c84f7042044eef2a29138caa3e00a2bee1985147
EmileSean <emilesean.es@gmail.com>
2024-05-09 20:45:12 +0300

    Update POC template

--- src/Poc-template.sol
+++ src/Poc-template.sol
@@ -1,23 +1,19 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 /*
 
 import "forge-std/Test.sol";
 import "./interface.sol";
 
-// @KeyInfo - Total Lost : ~999M US$
-// Attacker : 0xcafebabe
-// Attack Contract : 0xdeadbeef
-// Vulnerable Contract : 0xdeadbeef
+// @KeyInfo 
+// Project Name:
+// Date: 
+// Total Lost : ~999M US$
+// Vulenerability:
+// Description:
 // Attack Tx : 0x123456789
-
-// @Info
-// Vulnerable Contract Code : https://etherscan.io/address/0xdeadbeef#code
-
-// @Analysis
 // Post-mortem : https://www.google.com/
-// Twitter Guy : https://www.google.com/
-// Hacking God : https://www.google.com/
+// Source : https://www.google.com/
 
 contract ContractTest is Test {
     IUSDC constant USDC = IUSDC(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
