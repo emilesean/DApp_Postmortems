f87fb889b7ee2d3abdafb423f294a44cc8e13535
EmileSean <emilesean.es@gmail.com>
2024-05-11 21:29:23 +0300

    Update Interfaces

--- src/interfaces/ICurvePool.sol
+++ src/interfaces/ICurvePool.sol
@@ -3,118 +3,124 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 interface ICurvePool {
-    event AddLiquidity(
-        address indexed provider,
-        uint256[4] token_amounts,
-        uint256[4] fees,
-        uint256 invariant,
-        uint256 token_supply
-    );
-    event CommitNewAdmin(uint256 indexed deadline, address indexed admin);
-    event CommitNewParameters(
-        uint256 indexed deadline,
-        uint256 A,
-        uint256 fee,
-        uint256 admin_fee
-    );
-    event NewAdmin(address indexed admin);
-    event NewParameters(uint256 A, uint256 fee, uint256 admin_fee);
-    event RemoveLiquidity(
-        address indexed provider,
-        uint256[4] token_amounts,
-        uint256[4] fees,
-        uint256 token_supply
-    );
-    event RemoveLiquidityImbalance(
-        address indexed provider,
-        uint256[4] token_amounts,
-        uint256[4] fees,
-        uint256 invariant,
-        uint256 token_supply
-    );
-    event TokenExchange(
-        address indexed buyer,
-        int128 sold_id,
-        uint256 tokens_sold,
-        int128 bought_id,
-        uint256 tokens_bought
-    );
-    event TokenExchangeUnderlying(
-        address indexed buyer,
-        int128 sold_id,
-        uint256 tokens_sold,
-        int128 bought_id,
-        uint256 tokens_bought
-    );
-
     function A() external view returns (uint256 out);
+
+    function add_liquidity(
+        uint256[2] memory amounts,
+        uint256 min_mint_amount
+    ) external payable returns (uint256);
+
+    function add_liquidity(
+        uint256[3] memory amounts,
+        uint256 min_mint_amount
+    ) external returns (uint256);
+
     function add_liquidity(
         uint256[4] memory amounts,
         uint256 min_mint_amount
-    ) external;
-    function admin_actions_deadline() external view returns (uint256 out);
+    ) external returns (uint256);
+
     function admin_fee() external view returns (uint256 out);
-    function apply_new_parameters() external;
-    function apply_transfer_ownership() external;
-    function balances(int128 arg0) external view returns (uint256 out);
+
+    function balances(uint256 arg0) external view returns (uint256 out);
+
     function calc_token_amount(
-        uint256[4] memory amounts,
-        bool deposit
-    ) external view returns (uint256 out);
+        uint256[] memory amounts,
+        bool is_deposit
+    ) external view returns (uint256 lp_tokens);
+
+    /// @dev vyper upgrade changed this on us
     function coins(int128 arg0) external view returns (address out);
-    function commit_new_parameters(
-        uint256 amplification,
-        uint256 new_fee,
-        uint256 new_admin_fee
-    ) external;
-    function commit_transfer_ownership(address _owner) external;
-    function exchange(int128 i, int128 j, uint256 dx, uint256 min_dy) external;
+
+    /// @dev vyper upgrade changed this on us
+    function coins(uint256 arg0) external view returns (address out);
+
+    /// @dev vyper upgrade changed this on us
+    function underlying_coins(int128 arg0) external view returns (address out);
+
+    /// @dev vyper upgrade changed this on us
+    function underlying_coins(uint256 arg0) external view returns (address out);
+
+    function exchange(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy
+    ) external payable;
+
+    // newer pools have this improved version of exchange_underlying
+    function exchange(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy,
+        address receiver
+    ) external returns (uint256);
+
+    function exchange(
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy,
+        bool use_eth,
+        address receiver
+    ) external returns (uint256);
+
     function exchange_underlying(
         int128 i,
         int128 j,
         uint256 dx,
         uint256 min_dy
     ) external;
+
+    function exchange_underlying(
+        address pool,
+        int128 i,
+        int128 j,
+        uint256 dx,
+        uint256 min_dy
+    ) external;
+
     function fee() external view returns (uint256 out);
+
     function future_A() external view returns (uint256 out);
-    function future_admin_fee() external view returns (uint256 out);
+
     function future_fee() external view returns (uint256 out);
-    function future_owner() external view returns (address out);
-    function get_dx(
-        int128 i,
-        int128 j,
-        uint256 dy
-    ) external view returns (uint256 out);
-    function get_dx_underlying(
-        int128 i,
-        int128 j,
-        uint256 dy
-    ) external view returns (uint256 out);
+
+    function future_admin_fee() external view returns (uint256 out);
+
     function get_dy(
         int128 i,
         int128 j,
         uint256 dx
-    ) external view returns (uint256 out);
+    ) external view returns (uint256);
+
     function get_dy_underlying(
         int128 i,
         int128 j,
         uint256 dx
-    ) external view returns (uint256 out);
+    ) external view returns (uint256);
+
     function get_virtual_price() external view returns (uint256 out);
-    function kill_me() external;
-    function owner() external view returns (address out);
+
     function remove_liquidity(
-        uint256 _amount,
-        uint256[4] memory min_amounts
-    ) external;
+        uint256 token_amount,
+        uint256[2] memory min_amounts
+    ) external returns (uint256[2] memory);
+
+    function remove_liquidity(
+        uint256 token_amount,
+        uint256[3] memory min_amounts
+    ) external returns (uint256[3] memory);
+
     function remove_liquidity_imbalance(
-        uint256[4] memory amounts,
+        uint256[3] memory amounts,
         uint256 max_burn_amount
     ) external;
-    function revert_new_parameters() external;
-    function revert_transfer_ownership() external;
-    function transfer_ownership_deadline() external view returns (uint256 out);
-    function underlying_coins(int128 arg0) external view returns (address out);
-    function unkill_me() external;
-    function withdraw_admin_fees() external;
+
+    function remove_liquidity_one_coin(
+        uint256 token_amount,
+        int128 i,
+        uint256 min_amount
+    ) external;
 }
