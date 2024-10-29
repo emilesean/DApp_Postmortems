2a2eb73ca2ff6323f86db208196fa23665fbe9ce
Emile Sean <emilesean.es@gmail.com>
2024-10-29 09:08:01 +0300

    Refactor code and update POC template file
    - Renamed the POC template file from "<POC-Name-Template>.t.sol" to "POCTemplate.t.sol"
    - Updated the Total Lost value in the POC template to "~M USD$"
    - Added a TLDR description field in the POC template
    - Added a new function "testExploit" in the ContractTest contract to view the balance of USDC for the "alice" address

--- src/POCTempalate.t.sol
+++ src/POCTempalate.t.sol
@@ -5,12 +5,11 @@ pragma solidity ^0.8.10;
  * Key Information
  *  Project Name:
  *  Date:
- *  Total Lost : ~999M US$
+ *  Total Lost : ~M USD$
+ *  Vulenerability Category:
  *  Vulenerability:
- *  Description:
- *  Attack Tx : 0x123456789
- *  Post-mortem : https://www.google.com/
- *  Source : https://www.google.com/
+ *  TLDR Description:
+ *  Attack Tx1 :
  */
 
 /**
@@ -25,17 +24,21 @@ contract ContractTest is Test {
 
     IUSDC constant USDC = IUSDC(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
     IUSDT constant USDT = IUSDT(0xdAC17F958D2ee523a2206206994597C13D831ec7);
+    address public alice;
+    address public bob;
 
     function setUp() public {
         vm.createSelectFork("mainnet", 15_460_093);
         vm.label(address(USDC), "USDC");
         vm.label(address(USDT), "USDT");
-        //address alice = makeAddr("alice");
+        alice = makeAddr("alice");
+        bob = makeAddr("bob");
     }
 
-    function testExploit() public {
+    function testExploit() public view {
         //vm.startPrank(alice);
         //vm.stopPrank();
+        USDC.balanceOf(address(alice));
     }
 
 }
