4e49f4ffcd3484588588266990abd84c20d6dae0
Emile Sean <emilesean.es@gmail.com>
2024-10-29 09:54:17 +0300

    Refactor code and update POC template file

--- src/POCTempalate.t.sol
+++ src/POCTempalate.t.sol
@@ -2,25 +2,22 @@
 pragma solidity ^0.8.10;
 
 /**
- * Key Information
- *  Project Name:
- *  Date:
- *  Total Lost : ~M USD$
- *  Vulenerability Category:
- *  Vulenerability:
- *  TLDR Description:
- *  Attack Tx1 :
- */
-
-/**
- * Interfaces
+ * @title: Proof of Concept for Uniswapv4 Security breach
+ * @author: @emilesean_es
+ * @custom: Project Name: Uniswapv4
+ * @custom: Description:
+ * @custom: Date: Oct-29-2024 06:29:11 AM UTC
+ * @custom: Value lost:  ~1M USD$
+ * @custom: Vulenerability category: Arithimetic & Calculation Errors
+ * @custom: Attack Variant: CompoundFork
+ * @custom: [Attack Tx1:](https://etherscan.io/tx/0hx176bd09366ceb30c54dh1c0bb79065498dfcb3cc8d4967d2c7602247ec3c3bc44)
  */
 import "forge-std/Test.sol";
 
 import {IUSDC} from "src/interfaces/IUSDC.sol";
 import {IUSDT} from "src/interfaces/IUSDT.sol";
 
-contract ContractTest is Test {
+contract PocTemplate is Test {
 
     IUSDC constant USDC = IUSDC(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
     IUSDT constant USDT = IUSDT(0xdAC17F958D2ee523a2206206994597C13D831ec7);
